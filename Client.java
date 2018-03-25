d17d5806c5f458fec218e2cc52e69f99ddf1ee5f
oscardoudou <oscar9591@icloud.com>
2018-03-25 19:08:45 -0400

    chat but need to wait

--- Client.java
+++ Client.java
@@ -6,9 +6,11 @@ public class Client{
 		try{
 			String serverSentence;
 			String capitalizedSentence;
+			String clientSentence;
 			 
 			while(true){
 			Socket clientSocket = new Socket("127.0.0.1", 5678);
+			
 			DataInputStream inputStreamFromServer = new DataInputStream(clientSocket.getInputStream());
 			InputStreamReader serverStreamReader = new InputStreamReader(inputStreamFromServer);
 			BufferedReader inFromServer = new BufferedReader(serverStreamReader);
@@ -16,12 +18,16 @@ public class Client{
 			serverSentence = inFromServer.readLine();
 			System.out.println("From server: " + serverSentence);
 
+			BufferedReader outToServer = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Enter string to send to server:");
+			clientSentence = outToServer.readLine();
 			DataOutputStream outputStreamToServer = new DataOutputStream(clientSocket.getOutputStream());
 
-			capitalizedSentence = serverSentence.toUpperCase();
-			outputStreamToServer.writeBytes(capitalizedSentence + '\n');
+			//capitalizedSentence = serverSentence.toUpperCase();
+			outputStreamToServer.writeBytes(clientSentence + '\n');
+			//outputStreamToServer.writeBytes(capitalizedSentence + '\n');
 
-			System.out.println("Here is the capitalized result sent to server: " + capitalizedSentence);
+			//System.out.println("Here is the capitalized result sent to server: " + capitalizedSentence);
 			clientSocket.close();
 			}
 			// 
