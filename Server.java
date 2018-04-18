f822576e3d1dfcb60e09457c4eb0e26113a0bde0
oscardoudou <oscar9591@icloud.com>
2018-04-18 05:25:45 -0400

    parse ADD type of request and support variable rfc_no

--- Server.java
+++ Server.java
@@ -1,23 +1,23 @@
-package project;
 import java.net.*;
 import java.io.*;
 
 public class Server{
-	public static void main (String[] args)throws Exception{
-		try{
-			//create a server-side socket,namely ServerSocket, assign binding port
-			ServerSocket welcomeSocket = new ServerSocket(5678);  
-			while(true){
-			//accept() listen to port, wait for clients' connection	
-			Socket connectionSocket = welcomeSocket.accept();
-			//create a new thread
-			ServerThread serverThread = new ServerThread(connectionSocket);
-			serverThread.start();
-			}
-		}
-		catch(IOException e){
-			System.out.println(e);
-		}
-	}
-	
+
+    public static void main (String[] args)throws Exception{
+        try{
+            //create a server-side socket,namely ServerSocket, assign binding port
+            ServerSocket welcomeSocket = new ServerSocket(5678);
+            while(true){
+                //accept() listen to port, wait for clients' connection
+                Socket connectionSocket = welcomeSocket.accept();
+                //create a new thread
+                ServerThread serverThread = new ServerThread(connectionSocket);
+                serverThread.start();
+            }
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+    }
+
 }
--- ServerThread.java
+++ ServerThread.java
@@ -1,36 +1,98 @@
-package project;
 import java.io.*;
 import java.net.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ServerThread extends Thread {
-	Socket socket = null;
-	public ServerThread(Socket socket){
-		this.socket = socket;
-	}
-	public void run (){
-		String sentence;
-		String clientreply;
-		try{
-			DataInputStream inputStreamFromServerUser = new DataInputStream(System.in);
-			InputStreamReader serverUserStreamReader = new InputStreamReader(inputStreamFromServerUser);
-			BufferedReader inFromServerUser = new BufferedReader(serverUserStreamReader);
-			
-			System.out.println("Enter string to send to client:");
 
-			DataOutputStream outputStreamToClient = new DataOutputStream(socket.getOutputStream());
-			sentence = inFromServerUser.readLine();
+    public static List<PeerInfo> list = new LinkedList<>();
+    public static List<RFCInfo> index = new LinkedList<>();
 
-			outputStreamToClient.writeBytes(sentence + '\n');
+    String[] request = new String[5];
+    Socket socket = null;
+    //response should be initialize as "" not null, latter would cause null ahead of message
+    String response = "";
+    String version;
 
-			DataInputStream inputStreamFromClient = new DataInputStream(socket.getInputStream());
-			InputStreamReader clientStreamReader = new InputStreamReader(inputStreamFromClient);
-			BufferedReader inFromClient = new BufferedReader(clientStreamReader);
+    public ServerThread(Socket socket){
+        this.socket = socket;
+    }
 
-			clientreply = inFromClient.readLine();
-			System.out.println("From client: " + clientreply);
-		}
-		catch(IOException e){
-			System.out.println(e);
-		}
-	}
+    public void run (){
+        try{
+            String client_ip = socket.getRemoteSocketAddress().toString();
+            int client_port_no = socket.getPort();
+            System.out.println("Connecting  to "+ client_ip);
+
+            DataInputStream inputStreamFromClient = new DataInputStream(socket.getInputStream());
+            InputStreamReader clientStreamReader = new InputStreamReader(inputStreamFromClient);
+            BufferedReader inFromClient = new BufferedReader(clientStreamReader);
+
+            System.out.println("Request from client: ");
+            String line;
+            int i = 0;
+            while((line = inFromClient.readLine())!= null){
+                if("END".equals(line))
+                    break;
+                request[i++] = line;
+//                if(i <= 3)
+//                    System.out.println(request[0].substring(0,3));
+                System.out.println(line);
+            }
+            version = request[0].substring(request[0].length()-10,request[0].length());
+            //System.out.println(request[0].substring(0,3));
+            switch(request[0].substring(0,3)){
+                case "ADD":
+                    echoBack(request);
+                    break;
+                case "LOO":
+                    break;
+                case "LIS":
+                    break;
+                default:
+                    System.out.println("can not parse request type");
+                    break;
+            }
+
+            BufferedReader outToClient = new BufferedReader(new StringReader(response));
+            DataOutputStream outputStreamToClient = new DataOutputStream(socket.getOutputStream());
+            line = "";
+            while((line = outToClient.readLine())!=null){
+                //since line has get rid of \r\n to format multiple line into hierarchic message
+                outputStreamToClient.writeBytes(line + '\n');
+            }
+
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+    }
+    public void echoBack(String[] request){
+        String hostname = request[1].substring(6);
+        int port_no = Integer.parseInt(request[2].substring(6));
+        //int no = Integer.parseInt(request[0].substring(8,12));
+        //solve different rfc number length
+        String temp = request[0].substring(8,9);
+        int i = 9;
+        while(request[0].charAt(i) != ' ')
+            temp += request[0].charAt(i++);
+        int no = Integer.parseInt(temp);
+        String title = request[3].substring(7);
+        //here don't check if hostname duplicate, we solve this later when actually return list
+        //来之不拒就是加
+        list.add(new PeerInfo(hostname, port_no));
+        index.add(new RFCInfo(no, title, hostname));
+        response += version + " 200 OK\r\n";
+        response += "RFC " + no + " " + title + " " + hostname + " " + port_no +"\r\n";
+        response += "\r\n"+"END";
+//        Iterator it = list.iterator();
+//        while(it.hasNext()){
+//            System.out.println(it.next());
+//        }
+//        it = index.iterator();
+//        while(it.hasNext()){
+//            System.out.println(it.next());
+//        }
+    }
 }
