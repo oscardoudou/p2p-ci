6042eae6983a36b1e1efd6b9a14a27233f97e6df
oscardoudou <oscar9591@icloud.com>
2018-04-24 20:16:58 -0400

    refractor PeerClient and ignore executable file

--- p2pclient/PeerClient.java
+++ p2pclient/PeerClient.java
@@ -12,96 +12,137 @@ import java.util.Scanner;
  */
 
 public class PeerClient {
+
     Socket socket = null;
     String request = "";
     String hostname;
     String rfc_no = "";
+    int portno;
+    String[] response = new String[7];
+    int status;
+    int rfc_legnth;
 
     public void run(){
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter client's ip you want to reach:");
         hostname  = sc.nextLine();
         System.out.println("Specify the upload port no:");
-        int portno = Integer.parseInt(sc.nextLine());
+        portno = Integer.parseInt(sc.nextLine());
 
         try{
             socket =  new Socket(hostname,portno);
             System.out.println("Connection should be created");
 
             while (true) {
-                System.out.println("Whether you want to request rfc from Peer" + "[" + hostname + ":" + portno + "]");
+                System.out.println("Whether you want to get rfc from Peer" + "[" + hostname + ":" + portno + "](enter yes to get)");
                 String choice = sc.nextLine();
 
                 switch (choice) {
                     case "yes":
                         System.out.println("Input the rfc you want to get:");
                         rfc_no = sc.nextLine();
-                        get();
+                        constructRequest();
                         break;
                     default:
                         socket.close();
                         System.out.println("socket closed");
                         return;
                 }
-                String line;
-                DataOutputStream outputStreamToPeer = new DataOutputStream(socket.getOutputStream());
-                DataInputStream inputStreamFromPeer = new DataInputStream(socket.getInputStream());
-                BufferedReader outToPeer = new BufferedReader(new StringReader(request));
-                request = "";
+                
+                sendRequest(socket);
 
-                while ((line = outToPeer.readLine()) != null) {
-                    //if (line.equals("END"))
-                    //  break;
-                    //这里读进来的line没有\r\n
-                    outputStreamToPeer.writeBytes(line + "\n");
-                }
+                printResponse(response,socket);
 
-                System.out.println("Response from PeerServer: ");
-
-                String[] response = new String[7];
-                int i = 0;
-                line = "";
-                BufferedReader inFromPeer = new BufferedReader(new InputStreamReader(inputStreamFromPeer));
-                while ((line = inFromPeer.readLine()) != null){
-                    if (line.equals("END"))
-                        break;
-                    response[i++] = line;
-                    System.out.println(line);
-                }
-
-                int status = Integer.parseInt(response[0].substring(11,14));
-                int rfc_legnth = Integer.parseInt(response[4].substring(16,21));
-                if(status == 200){
-                    File curdir = new File(System.getProperty("user.dir"));
-                    // String parentPath = curdir.getCanonicalFile().getParent();
-                    // parentPath += "/rfc";
-                    System.out.println(System.getProperty("user.dir"));
-                    //server filelist循环时貌似一定要点 这里不循环可以不用
-                    BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
-                    byte[] bytearray = new byte[rfc_legnth];
-                    bis.read(bytearray,0,bytearray.length);
-
-                    FileOutputStream fileOutputStream = new FileOutputStream(curdir.getCanonicalPath() + "/rfc/rfc" + rfc_no + ".txt");
-                    BufferedOutputStream bos = new BufferedOutputStream(fileOutputStream);
-                    bos.write(bytearray,0,bytearray.length);
-                }
+                receiveRFC(response,socket);
 
             }
 
         }catch(Exception e){
-//            e.printStackTrace();
             System.out.println(e);
         }
 
     }
-    public void get(){
 
-            request += "GET RFC " + rfc_no + " P2P-CI/1.0" + "\r\n";
+    public void constructRequest(){
+
+        request += "GET RFC " + rfc_no + " P2P-CI/1.0" + "\r\n";
             //hostname is from which RFC is requested
-            request += "Host: " + hostname + "\r\n";
+        request += "Host: " + hostname + "\r\n";
             //OS of requesting host
-            request += "OS: " + System.getProperty("os.name") + " " + System.getProperty("os.version") + "\r\n";
-            request += "\r\n" + "END";
+        request += "OS: " + System.getProperty("os.name") + " " + System.getProperty("os.version") + "\r\n";
+        request += "\r\n" + "END";
+
+    }
+
+    public void sendRequest(Socket socket){
+        String line;
+        try{
+            DataOutputStream outputStreamToPeer = new DataOutputStream(socket.getOutputStream());
+            BufferedReader outToPeer = new BufferedReader(new StringReader(request));
+            //reset request ?
+            request = "";
+
+            while ((line = outToPeer.readLine()) != null) {
+                    //if (line.equals("END"))
+                    //  break;
+                    //这里读进来的line没有\r\n
+                outputStreamToPeer.writeBytes(line + "\n");
+        }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
+    } 
+
+    public void printResponse(String[] response, Socket socket){
+        System.out.println("Response from PeerServer: ");
+        int i = 0;
+        String line = "";
+        try{
+
+            BufferedReader inFromPeer = new BufferedReader(new InputStreamReader(new DataInputStream(socket.getInputStream())));
+            while ((line = inFromPeer.readLine()) != null){
+                if (line.equals("END"))
+                    break;
+                response[i++] = line;
+                System.out.println(line);
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
+    }
+
+    public void receiveRFC(String[] response, Socket socket){
+
+        status = Integer.parseInt(response[0].substring(11,14));
+        rfc_legnth = Integer.parseInt(response[4].substring(16,21));
+
+        if(status == 200){
+
+            File curdir = new File(System.getProperty("user.dir"));
+            // String parentPath = curdir.getCanonicalFile().getParent();
+            // parentPath += "/rfc";
+            //server filelist循环时貌似一定要点 这里不循环可以不用
+            //System.out.println(System.getProperty("user.dir"));
+            
+            try{
+
+                BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+                byte[] bytearray = new byte[rfc_legnth];
+                bis.read(bytearray,0,bytearray.length);
+
+                FileOutputStream fileOutputStream = new FileOutputStream(curdir.getCanonicalPath() + "/rfc/rfc" + rfc_no + ".txt");
+                BufferedOutputStream bos = new BufferedOutputStream(fileOutputStream);
+                bos.write(bytearray,0,bytearray.length);
+
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            
+        }
 
     }
 }
